501f52cfbb17fc3854e9c2a98f5fa3832c07932c
BONK777 <79047358+BONK777@users.noreply.github.com>
2021-11-24 17:00:09 +0300

    add config

--- server/routes/mainRouter.js
+++ server/routes/mainRouter.js
@@ -2,17 +2,31 @@ const router = require("express").Router();
 const fs = require("fs");
 
 let data = "";
-const readFile = async (path) => {
-    data = await fs.readFile(path, "utf-8", (err, info) => {return info});
-    console.log(data);
+const readFile = (path) => {
+    return fs.readFileSync(path, "utf-8");
 }
 
-readFile("./data/fruit.cvs");
+data = readFile("./data/fruit.cvs");
+data = data.split("\s\c")
 console.log(data);
+
+const Product = (prArr) => {
+    let names = data[0].split(";");
+    names.forEach((name, idx) => {this[name] = prArr[idx]})   
+}
+
+let first = new Product(data[1].split(";"));
+console.log(first);
+const products = [];
+for(let i = 1; i < data.length; i++) {
+    products.push(new Product(data[i].split(";")));
+}
+
 
 router.get("/", (req, res) => {
     res.render("index", {
-        title: "Одевайся с ХАЙПОМ!"
+        title: "Одевайся с ХАЙПОМ!",
+        products: products
     });
 });
 
